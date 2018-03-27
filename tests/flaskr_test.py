1cb70d7ebd656e3d5595b7c38577cd4377a028b6
Shalon Conley <scc2174@barnrd.edu>
2018-03-26 23:43:53 -0400

    the nth fix

--- tests/flaskr_test.py
+++ tests/flaskr_test.py
@@ -1,19 +1,14 @@
 import os
 import unittest
-import sqlalchemy
+import testing.postgresql
 
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.postgresql = testing.postgresql.Postgresql()
 
-class TestDB(unittest.TestCase):
-	"""
-	will try to fetch the database URL to use from an environment variable, and
-	then wiill rely on SQLAlchemy to create a database conncection 
-	"""  
-	def setUp(self):
-		url = os.getenv("DB_TEST_URL")
-		if not url:
-			self.skipTest("No database URl set")
-		self.engine = sqlalchemy.create_engine(url)
-
+    def tearDown(self):
+        self.postgresql.stop()
+	
 	def test_empty_db(self):
 		rv = self.app.get('/')
 		assert 'No entries here so far' in rv.data
