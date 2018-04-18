36ee906984514d6a29a98174124de88852fe78d9
Shahar Kaminsky <ShaharKaminsky@dyn-160-39-202-196.dyn.columbia.edu>
2018-04-18 01:06:41 -0400

    Added a page for logged in users to schedule or cancel a workout and connected it to the flask framework.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from flask import render_template
-from flask import request 
+from flask import request
 
 import fix_path
 
 from models.users import User
 from models.machines import Machine
-from models import Base_Model as bm 
+from models import Base_Model as bm
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -18,7 +17,7 @@ def index():
     #return render_template("index.html")
 
 #placeholder function
-@app.route('/login', methods=['POST'])
+@app.route('/LoggedInUsers', methods=['POST'])
 def login():
 	error = None
 	if request.method == "POST":
@@ -26,7 +25,7 @@ def login():
 		res = user.addUser()
 
 		if res == 1:
-			return render_template("machineDayschedule.html",error=error)
+			return render_template("LoggedInUsers.html",error=error)
 		else:
 			error = "invalid username/password"
 
@@ -34,8 +33,8 @@ def login():
 
 	mg = Machine(bm.Base_Model())
 	machines = mg.get_all_machines()
-	
-	return render_template("machineDayschedule.html",error=error, machines=machines)
+
+	return render_template("LoggedInUsers.html",error=error, machines=machines)
 
 def signin():
 	error = None
@@ -46,11 +45,11 @@ def signin():
 		user.db_close()
 
 		if res == 1:
-			return render_template("machineDayschedule.html",error=error)
+			return render_template("LoggedInUsers.html",error=error)
 		else:
 			error = "invalid username/password"
-	
-	
+
+
 @app.route('/machine_schedule')
 def machine_schedule():
 	#machine = Machine()
@@ -60,6 +59,10 @@ def machine_schedule():
 @app.route('/overall_schedule')
 def overall_schedule():
 	return render_template("overallDaySchedule.html")
+
+@app.route('/LoggedInUsers')
+def LoggedInUsers():
+    return render_template("LoggedInUsers.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
