5a2cdc040c545b264968950bba329b55fc9de32f
shaconley21 <scc2174@barnard.edu>
2018-04-20 22:11:29 -0400

    features functions for availbility and reservations

--- src/models/schedules.py
+++ src/models/schedules.py
@@ -8,16 +8,45 @@ class Schedule(Base_Model):
 
 		self.conn = self.db_connect()
 		self.cur = self.conn.cursor()
+		self.userid = userid
+		self.machineid = machineid
 
 		#function to find out if a time slot for a certain machine is free or full
-		def is_available(self,time):
+		def is_available(self):
 			err = 1
 			try:
+				available = self.cur.execute("SELECT * FROM machines WHERE machineid IS NULL;")
+				if(available == NULL):
+					return True
+				else:
+					return False
 				#TODO find correct sql query
-				self.cur.execute("SELECT * FROM schedules WHERE time ="+str(time))
+				#self.cur.execute("SELECT * FROM schedules WHERE time ="+str(time))
 			except:
 				print("could not insert into db")
 				err = 0
+		
 		def reserve_time_slot(self,time):
 			err = 1
 			self.cur.execute("INSERT INTO schedules VALUES")
+
+		def make_reservation(userid,machineid):
+			try:
+				availbility = is_available(self)
+				if availability == True:
+					self.cur.execute("INSERT INTO schedules VALUES (" + ("'") + (self.userid) +("'") +(", '")+ (self.machineid) + ("',"))
+					return print("Your machine is reserved")
+			except:
+				print("could no insert into db")
+				err = 0 
+		
+		def cancel_reservation(userid,machineid):
+			err = 1
+			try:
+				self.cur.execute(DELETE FROM schedules WHERE userid=+self.userid AND machineid=+self.machineid)
+			except:
+				print("could not execute statement")
+				err = 0
+				
+			return err
+
