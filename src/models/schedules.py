ed26d6f0a89543c9fc2a0e6d15f85863160e96db
Leon Stilwell <leostilwell@gmail.com>
2018-04-23 01:08:52 -0400

    fixed bug in schedules

--- src/models/schedules.py
+++ src/models/schedules.py
@@ -2,7 +2,7 @@
 
 from users import User
 
-class Schedule(Base_Model):
+class Schedule():
 	def __init__(self,machineid):
 		Base_Model.__init__(self)
 
@@ -12,26 +12,26 @@ class Schedule(Base_Model):
 
 		#function to find out if a time slot for a certain machine is free or full
 		def is_available(self):
-			err = 1
 			try:
-				available = self.cur.execute("SELECT * FROM machines WHERE machineid IS NULL;")
-				if(available == NULL):
+				available = self.cur.execute("SELECT * FROM machines WHERE machineid IS NULL")
+				records = cur.fetchall()
+				if(len(records) is 0):
 					return True
 				else:
 					return False
 				#TODO find correct sql query
 				#self.cur.execute("SELECT * FROM schedules WHERE time ="+str(time))
-			except:
-				print("could not insert into db")
-				err = 0
+			except Exception as e:
+				print(e)
+			
 		
-		def reserve_time_slot(self,time):
-			err = 1
-			self.cur.execute("INSERT INTO schedules VALUES")
+		#def reserve_time_slot(self,time):
+		#	err = 1
+		#	self.cur.execute("INSERT INTO schedules VALUES (%s,%s,%s) ", (self.user_id,self.machineid,time))
 
-		def make_reservation(userid,machineid):
+		def make_reservation(userid,machineid,time):
 			try:
-				availbility = is_available(self)
+				availbility = self.is_available()
 				if availability == True:
 					self.cur.execute("INSERT INTO schedules VALUES (" + ("'") + (userid) +("'") +(", '")+ (machineid) + ("',"))
 					return print("Your machine is reserved")
