71ae1b780d2664ea4fb2eec610a3cca1a864a1a4
janukem <janukem@due.com>
2018-04-24 16:38:38 -0400

    Restructed tests

--- src/tests/schedule/test_schedule_appointment.py
+++ src/tests/schedule/test_schedule_appointment.py
@@ -13,9 +13,7 @@ class MyTestCase(unittest.TestCase):
 
 	def test_func(self):
 		res = self.schedule.get_all_appointments()
-		print(res)
-		#self.assertEqual(res, ["08:00 - 08:30","08:30 - 09:00","09:00 - 09:30","10:00 - 10:00","10:30 - 11:00","11:00 - 11:30","11:30 - 12:00","12:30 - 13:00","13:00 - 13:30","13:30 - 14:00","14:00 - 14:30","14:30 - 15:00","15:00 - 15:30","15:30 - 16:00","16:00 - 16:30","16:30 - 17:00"])
-
+		assert(len(res) != 0)
 		
 	def tearDown(self):
 		self.schedule.db_close()
